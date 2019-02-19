9af02705dbd14eaa84496e3c3f75f8cb3d39c3c6
Masha Blair <mashablair@gmail.com>
2019-02-19 09:59:36 -0800

    Add Jumbotron and Container components from react-bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="">
-
+        <Jumbotron>
+          <Container>
+            <h1>Search App</h1>
+            <p>
+              This is a simple search app.
+            </p>
+          </Container>
+        </Jumbotron>;
       </div>
     );
   }
